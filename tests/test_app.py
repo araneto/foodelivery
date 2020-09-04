fd0f75c80045f33cba26b94bd62810ea5ae77b0c
Alberto Andrade <araneto.ctba@gmail.com>
2020-09-04 14:51:49 -0300

    Add clean and test to Makefile

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,11 @@
 
 def test_app_is_created(app):
     assert app.name == 'foodelivery.app'
+
+
+def test_config_is_loaded(config):
+    assert config["DEBUG"] is False
+
+
+def test_request_returs_404(client):
+    assert client.get('/url_que_nao_existe').status_code == 404
