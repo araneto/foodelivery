9fec72e68d7a03910960ed21fd942f6eb52d14eb
Alberto Andrade <araneto.ctba@gmail.com>
2020-09-04 17:41:05 -0300

    Add 1st blueprint

--- foodelivery/app.py
+++ foodelivery/app.py
@@ -1,13 +1,16 @@
 """
+: Foodelivery App Entry point
 export FLASK_ENV=development
 export FLASK_APP=foodelivery/app.py
 """
 
 
 from flask import Flask
+from foodelivery.ext import site
 
 
 def create_app():
     """Factory to create a Flask app based on fatory pattern"""
     app = Flask(__name__)
+    site.init_app(app)
     return app
---
+++ foodelivery/ext/site/main.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+
+
+bp = Blueprint("site", __name__)
+
+
+@bp.route("/")
+def index():
+    return "Hello World"
