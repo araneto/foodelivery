c8afdb1a0231daddccdf43630408f453cffd8457
Alberto Andrade <araneto.ctba@gmail.com>
2020-09-11 11:30:00 -0300

    Add templates and debug toolbar

--- foodelivery/app.py
+++ foodelivery/app.py
@@ -7,10 +7,14 @@ export FLASK_APP=foodelivery/app.py
 
 from flask import Flask
 from foodelivery.ext import site
+from foodelivery.ext import toolbar
+from foodelivery.ext import config
 
 
 def create_app():
-    """Factory to create a Flask app based on fatory pattern"""
+    """Factory to create a Flask app based on factory pattern"""
     app = Flask(__name__)
+    config.init_app(app)
+    toolbar.init_app(app)
     site.init_app(app)
     return app
--- foodelivery/ext/site/main.py
+++ foodelivery/ext/site/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 
 bp = Blueprint("site", __name__)
@@ -6,4 +6,14 @@ bp = Blueprint("site", __name__)
 
 @bp.route("/")
 def index():
-    return "Hello World"
+    return render_template("index.html")
+
+
+@bp.route("/sobre")
+def about():
+    return render_template("about.html")
+
+
+@bp.route("/restaurantes")
+def restaurants():
+    return render_template("restaurants.html")
