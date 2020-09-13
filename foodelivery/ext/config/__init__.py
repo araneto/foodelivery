f65591f99cf4e2e482e81fb795e8b788913f071a
Alberto Andrade <araneto.ctba@gmail.com>
2020-09-13 20:03:03 -0300

    Database with sqlalchemy

--- foodelivery/ext/config/__init__.py
+++ foodelivery/ext/config/__init__.py
@@ -1,6 +1,6 @@
 def init_app(app):
     app.config["SECRET_KEY"] = "passW0rd01"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///foodelivery.db"
     if app.debug:
         app.config["DEBUG_TB_TEMPLATE_EDITOR_ENABLED"] = True
         app.config["DEBUG_TB_PROFILER_ENABLED"] = True
-
